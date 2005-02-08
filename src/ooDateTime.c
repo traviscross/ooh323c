9e76f66f1bfc3da054e4c41e826ee6a0ccd3beed
Vishal Phirke <vphirke@obj-sys.com>
2005-02-08 20:34:41 +0000

    Added DateTime

---
+++ src/ooDateTime.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2004-2005 by Objective Systems, Inc.
+ *
+ * This software is furnished under an open source license and may be
+ * used and copied only in accordance with the terms of this license.
+ * The text of the license may generally be found in the root
+ * directory of this installation in the LICENSE.txt file.  It
+ * can also be viewed online at the following URL:
+ *
+ *   http://www.obj-sys.com/open/license.html
+ *
+ * Any redistributions of this file including modified versions must
+ * maintain this copyright notice.
+ *
+ *****************************************************************************/
+
+#include "ooDateTime.h"
+
+#if defined(_WIN32)||defined(_MSC_VER) || defined(__MINGW32__)
+#include <SYS\TIMEB.H>
+int gettimeofday (struct timeval *tv, struct timezone *tz)
+{
+   struct _timeb currSysTime;
+   _ftime(&currSysTime);
+        
+   tv->tv_sec = currSysTime.time;
+   tv->tv_usec = currSysTime.millitm * 1000;
+
+   return 0;
+}
+#endif
+
+int ooGetTimeOfDay (struct timeval *tv, struct timezone *tz)
+{
+   return gettimeofday (tv, tz);
+}
