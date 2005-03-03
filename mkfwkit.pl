40ccf62bf26b0a01e77a40bbb7741777b4ddd536
Vishal Phirke <vphirke@obj-sys.com>
2005-03-03 23:04:20 +0000

    Updated

--- mkfwkit.pl
+++ mkfwkit.pl
@@ -20,6 +20,7 @@ print `cp -f ./COPYING ./ooh323c-${version}/COPYING`;
 print `cp -f ./ChangeLog ./ooh323c-${version}/ChangeLog`;
 print `cp -f ./INSTALL ./ooh323c-${version}/INSTALL`;
 print `cp -f ./Makefile.am ./ooh323c-${version}/Makefile.am`;
+print `cp -f ./Makefile.in ./ooh323c-${version}/Makefile.in`;
 print `cp -f ./NEWS ./ooh323c-${version}/NEWS`;
 print `cp -f ./README ./ooh323c-${version}/README`;
 
@@ -51,6 +52,7 @@ mkdir ("ooh323c-${version}/src/h323", 0777);
 #mkdir ("ooh323c-${version}/src/h450", 0777);
 
 print `cp -f ./src/Makefile.am ./ooh323c-${version}/src/Makefile.am`;
+print `cp -f ./src/Makefile.in ./ooh323c-${version}/src/Makefile.in`;
 print `cp -f ./src/oostk.dsp ./ooh323c-${version}/src/oostk.dsp`;
 print `cp -f ./src/*.h ./ooh323c-${version}/src`;
 print `cp -f ./src/*.c ./ooh323c-${version}/src`;
@@ -66,11 +68,13 @@ print `cp -f ./src/h323${suff}/*.c ./ooh323c-${version}/src/h323`;
 
 mkdir ("ooh323c-${version}/tests", 0777);
 print `cp -f ./tests/Makefile.am ./ooh323c-${version}/tests/Makefile.am`;
+print `cp -f ./tests/Makefile.in ./ooh323c-${version}/tests/Makefile.in`;
 
 mkdir ("ooh323c-${version}/tests/simple", 0777);
 print `cp -f ./tests/simple/*.c ./ooh323c-${version}/tests/simple`;
 print `cp -f ./tests/simple/*.h ./ooh323c-${version}/tests/simple`;
 print `cp -f ./tests/simple/Makefile.am ./ooh323c-${version}/tests/simple/Makefile.am`;
+print `cp -f ./tests/simple/Makefile.in ./ooh323c-${version}/tests/simple/Makefile.in`;
 print `cp -f ./tests/simple/simple.dsp ./ooh323c-${version}/tests/simple/simple.dsp`;
   
 
@@ -78,6 +82,7 @@ mkdir ("ooh323c-${version}/tests/player", 0777);
 print `cp -f ./tests/player/*.c ./ooh323c-${version}/tests/player`;
 print `cp -f ./tests/player/*.h ./ooh323c-${version}/tests/player`;
 print `cp -f ./tests/player/Makefile.am ./ooh323c-${version}/tests/player/Makefile.am`;
+print `cp -f ./tests/player/Makefile.in ./ooh323c-${version}/tests/player/Makefile.in`;
 print `cp -f ./tests/player/space.raw ./ooh323c-${version}/tests/player/space.raw`;
 print `cp -f ./tests/player/ooPlayer.dsp ./ooh323c-${version}/tests/player/ooPlayer.dsp`;
 print `cp -f ./tests/player/space.wav ./ooh323c-${version}/tests/player`;
@@ -87,6 +92,7 @@ mkdir ("ooh323c-${version}/tests/receiver", 0777);
 print `cp -f ./tests/receiver/*.c ./ooh323c-${version}/tests/receiver`;
 print `cp -f ./tests/receiver/*.h ./ooh323c-${version}/tests/receiver`;
 print `cp -f ./tests/receiver/Makefile.am ./ooh323c-${version}/tests/receiver/Makefile.am`;
+print `cp -f ./tests/receiver/Makefile.in ./ooh323c-${version}/tests/receiver/Makefile.in`;
 print `cp -f ./tests/receiver/ooReceiver.dsp ./ooh323c-${version}/tests/receiver/ooReceiver.dsp`;
   
 
@@ -94,6 +100,7 @@ mkdir ("ooh323c-${version}/media", 0777);
 print `cp -r -f ./media/*.c ./ooh323c-${version}/media`;
 print `cp -r -f ./media/*.h ./ooh323c-${version}/media`;
 print `cp -r -f ./media/Makefile.am ./ooh323c-${version}/media/Makefile.am`;
+print `cp -r -f ./media/Makefile.in ./ooh323c-${version}/media/Makefile.in`;
 print `cp -r -f ./media/oomedia.dsp ./ooh323c-${version}/media/oomedia.dsp`;
 
 
