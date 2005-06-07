9822977a2455e74a1a74f2c436b51ad214156ff9
Vishal Phirke <vphirke@obj-sys.com>
2005-06-07 22:16:42 +0000

    updated script

--- mkfwkit.pl
+++ mkfwkit.pl
@@ -30,6 +30,8 @@ mkdir ("ooh323c-${version}", 0777);
 print `cp -f ./AUTHORS ./ooh323c-${version}/AUTHORS`;
 print `cp -f ./COPYING ./ooh323c-${version}/COPYING`;
 print `cp -f ./ChangeLog ./ooh323c-${version}/ChangeLog`;
+
+#ooh323c directory
 if ($ARGV[0] eq "src") {
    print `cp -f ./INSTALL ./ooh323c-${version}/INSTALL`;
    print `cp -f ./Makefile.am ./ooh323c-${version}/Makefile.am`;
@@ -49,26 +51,36 @@ if ($ARGV[0] eq "src") {
    print `rm -rf ./ooh323c-${version}/config/CVS`;
 }
 print `cp -f ./README ./ooh323c-${version}/README`;
+
+
+#ooh323c docs
 mkdir ("ooh323c-${version}/doc", 0777);
-mkdir ("ooh323c-${version}/doc/H323StackLibrary", 0777);
-mkdir ("ooh323c-${version}/doc/MediaLibrary", 0777);
+mkdir ("ooh323c-${version}/doc/html", 0777);
 print `cp -f -r ./doc/H323Introduction.PDF ./ooh323c-${version}/doc/.`;
-print `cp -f -r ./doc/MediaLibRef0.2.PDF ./ooh323c-${version}/doc/.`;
 print `cp -f -r ./doc/ooH323cUsersGuide.pdf ./ooh323c-${version}/doc/.`;
-print `cp -f -r ./doc/ooH323cAPIRefMan.pdf ./ooh323c-${version}/doc/.`;
-print `cp -f -r ./doc/H323StackLibrary/* ./ooh323c-${version}/doc/H323StackLibrary`;
-print `rm -rf ./ooh323c-${version}/doc/H323StackLibrary/CVS`;
-print `cp -f -r ./doc/MediaLibrary/* ./ooh323c-${version}/doc/MediaLibrary`;
-print `rm -rf ./ooh323c-${version}/doc/MediaLibrary/CVS`;
-   
+print `cp -f -r ./doc/footer.shtml ./ooh323c-${version}/doc/.`;
+print `cp -f -r ./doc/header.shtml ./ooh323c-${version}/doc/.`;
+print `cp -f -r ./doc/stylesheet.css ./ooh323c-${version}/doc/.`;
+print `cp -f -r ./doc/html/* ./ooh323c-${version}/doc/html`;
+print `rm -rf ./ooh323c-${version}/doc/html/CVS`;
+
+
+#oomedia docs
+mkdir ("ooh323c-${version}/media", 0777);
+mkdir ("ooh323c-${version}/media/doc", 0777);
+mkdir ("ooh323c-${version}/media/doc/html", 0777);
+print `cp -f -r ./media/doc/MediaLibRef0.2.PDF ./ooh323c-${version}/media/doc/.`;
+print `cp -f -r ./media/doc/html/* ./ooh323c-${version}/media/doc/html/.`;
+print `rm -rf ./ooh323c-${version}/media/doc/html/CVS`;
+
+#specs
 mkdir ("ooh323c-${version}/specs", 0777);
 print `cp -f ./specs/*.asn ./ooh323c-${version}/specs`;
 
+#ooh323c library source files
 if ($ARGV[0] eq "src") {  
    mkdir ("ooh323c-${version}/src", 0777);
    mkdir ("ooh323c-${version}/src/h323", 0777);
-#mkdir ("ooh323c-${version}/src/h450", 0777);
-
    print `cp -f ./src/Makefile.am ./ooh323c-${version}/src/Makefile.am`;
    print `cp -f ./src/Makefile.in ./ooh323c-${version}/src/Makefile.in`;
    print `cp -f ./src/oostk.dsp ./ooh323c-${version}/src/oostk.dsp`;
@@ -79,45 +91,51 @@ if ($ARGV[0] eq "src") {
 }
 
 
-
+#create example directories
 mkdir ("ooh323c-${version}/tests", 0777);
 mkdir ("ooh323c-${version}/tests/simple", 0777);
 mkdir ("ooh323c-${version}/tests/player", 0777);
 mkdir ("ooh323c-${version}/tests/receiver", 0777);
 mkdir ("ooh323c-${version}/tests/chansetup", 0777);
 
+#copy example sources
 if ($ARGV[0] eq "src") {
+
+   #test dir
    print `cp -f ./tests/Makefile.am ./ooh323c-${version}/tests/Makefile.am`;
    print `cp -f ./tests/Makefile.in ./ooh323c-${version}/tests/Makefile.in`;
 
+   #simple
    print `cp -f ./tests/simple/*.c ./ooh323c-${version}/tests/simple`;
    print `cp -f ./tests/simple/*.h ./ooh323c-${version}/tests/simple`;
    print `cp -f ./tests/simple/Makefile.am ./ooh323c-${version}/tests/simple/Makefile.am`;
    print `cp -f ./tests/simple/Makefile.in ./ooh323c-${version}/tests/simple/Makefile.in`;
    print `cp -f ./tests/simple/simple.dsp ./ooh323c-${version}/tests/simple/simple.dsp`;
 
+   #player
    print `cp -f ./tests/player/*.c ./ooh323c-${version}/tests/player`;
    print `cp -f ./tests/player/*.h ./ooh323c-${version}/tests/player`;
    print `cp -f ./tests/player/Makefile.am ./ooh323c-${version}/tests/player/Makefile.am`;
    print `cp -f ./tests/player/Makefile.in ./ooh323c-${version}/tests/player/Makefile.in`;
-
    print `cp -f ./tests/player/ooPlayer.dsp ./ooh323c-${version}/tests/player/ooPlayer.dsp`;
    print `cp -f ./tests/player/space.wav ./ooh323c-${version}/tests/player`;
    print `cp -f ./tests/player/states.wav ./ooh323c-${version}/tests/player`;
    print `cp -f ./tests/player/space.raw ./ooh323c-${version}/tests/player/space.raw`;
 
+   #receiver
    print `cp -f ./tests/receiver/*.c ./ooh323c-${version}/tests/receiver`;
    print `cp -f ./tests/receiver/*.h ./ooh323c-${version}/tests/receiver`;
    print `cp -f ./tests/receiver/Makefile.am ./ooh323c-${version}/tests/receiver/Makefile.am`;
    print `cp -f ./tests/receiver/Makefile.in ./ooh323c-${version}/tests/receiver/Makefile.in`;
    print `cp -f ./tests/receiver/ooReceiver.dsp ./ooh323c-${version}/tests/receiver/ooReceiver.dsp`;
-  
 
+   #h323peer
    print `cp -f ./tests/chansetup/*.c ./ooh323c-${version}/tests/chansetup`;
    print `cp -f ./tests/chansetup/Makefile.am ./ooh323c-${version}/tests/chansetup/Makefile.am`;
    print `cp -f ./tests/chansetup/Makefile.in ./ooh323c-${version}/tests/chansetup/Makefile.in`;
    print `cp -f ./tests/chansetup/chansetup.dsp ./ooh323c-${version}/tests/chansetup/chansetup.dsp`;
 
+   #media library
    mkdir ("ooh323c-${version}/media", 0777);
    print `cp -r -f ./media/*.c ./ooh323c-${version}/media`;
    print `cp -r -f ./media/*.h ./ooh323c-${version}/media`;
@@ -126,21 +144,30 @@ if ($ARGV[0] eq "src") {
    print `cp -r -f ./media/oomedia.dsp ./ooh323c-${version}/media/oomedia.dsp`;
 }
 
+#unix binary build
 if ($ARGV[0] eq "unix") {
    mkdir (ooh323c-${version}/libs", 0777);
+   print `cp -f ./lib/ooh323c.a ./ooh323c-${version}/libs/ooh323c.a`;
+   print `cp -f ./lib/liboomedia.so.1.0.1 ./ooh323c-${version}/libs/.`;  
+   print `cp -f ./lib/liboomedia.so ./ooh323c-${version}/libs/.`;
+   print `cp -f ./tests/simple/simple ./ooh323c-${version}/tests/simple/.`;
+   print `cp -f ./tests/player/ooPlayer ./ooh323c-${version}/tests/player/.`;
    print `cp -f ./tests/player/space.raw ./ooh323c-${version}/tests/player/space.raw`;
+   print `cp -f ./tests/receiver/ooReceiver ./ooh323c-${version}/tests/receiver/.`;
+   print `cp -f ./tests/chansetup/h323peer ./ooh323c-${version}/tests/chansetup/.`;
 }
 
+# windows binary build
 if ($ARGV[0] eq "win") {
    mkdir (ooh323c-${version}/libs", 0777);
    print `cp -f ./libs/ooh323c_a.lib ./ooh323c-${version}/libs/ooh323c_a.lib`;
    print `cp -f ./libs/ooh323c.dll ./ooh323c-${version}/libs/ooh323c.dll`;
    print `cp -f ./libs/ooh323c.lib ./ooh323c-${version}/libs/ooh323c.lib`;
    print `cp -f ./tests/simple/simple.exe ./ooh323c-${version}/tests/simple/.`;
-   print `cp -f ./tests/player/player.exe ./ooh323c-${version}/tests/player/.`;
+   print `cp -f ./tests/player/ooPlayer.exe ./ooh323c-${version}/tests/player/.`;
    print `cp -f ./tests/player/space.wav ./ooh323c-${version}/tests/player`;
    print `cp -f ./tests/player/states.wav ./ooh323c-${version}/tests/player`;
-   print `cp -f ./tests/receiver/receiver.exe ./ooh323c-${version}/tests/receiver/.`;
+   print `cp -f ./tests/receiver/ooReceiver.exe ./ooh323c-${version}/tests/receiver/.`;
    print `cp -f ./tests/chansetup/h323peer.exe ./ooh323c-${version}/tests/chansetup/.`;
   
 }
